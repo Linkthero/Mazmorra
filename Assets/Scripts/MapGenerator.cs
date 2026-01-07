55008464498c596797101032185ab1b98cfda65d
Linkthero <amsdrgath@gmail.com>
2026-01-07 10:47:54 +0100

    mazomrra

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -5,136 +5,272 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
-    //Declaracion de clase interna
+    //    //Declaracion de clase interna
+    //    public class Cell
+    //    {
+    //        public bool visited = false; //Indica si la celda ha sido visitada
+    //        public bool[] status = new bool[4]; //0 - top; 1- down ; 2 - left; 3 - right
+    //    }
+
+    //    [SerializeField] private Vector2Int size;
+    //    [SerializeField] private GameObject room;
+    //    [SerializeField] private int initPosition = 0;
+    //    [SerializeField] private Vector2 roomSize;
+    //    [SerializeField] private int nRooms;
+
+    //    List<Cell> board;
+
+    //    private void Start()
+    //    {
+    //        MazeGenerator();
+    //    }
+
+    //    private void MazeGenerator()
+    //    {
+    //        board = new List<Cell>();
+
+    //        for (int i = 0; i < size.x; i++)
+    //        {
+    //            for (int j = 0; j < size.y; j++)
+    //            {
+    //                board.Add(new Cell());
+    //            }
+    //        }
+
+    //        int currentCell = initPosition;
+
+    //        Stack<int> path = new Stack<int>(); //Pila 
+
+    //        int num = 1;
+    //        while (num < nRooms)
+    //        {
+    //            board[currentCell].visited = true;
+
+    //            //Comprobar vecinos
+    //            List<int> neighbors = CheckNeighbors(currentCell);
+
+    //            if (neighbors.Count == 0)
+    //            { //no hay vecinos
+    //                if (path.Count == 0)
+    //                {
+    //                    break; //no hay mas celdas en el camino
+    //                }
+    //                else
+    //                {
+    //                    currentCell = path.Pop(); //Pop saca 1 elemento de la fila
+    //                }
+    //            }
+    //            else //si hay vecinos
+    //            {
+    //                path.Push(currentCell);
+
+    //                int newCell = neighbors[Random.Range(0, neighbors.Count)]; //elegir vecino aleatorio
+
+    //                if (newCell > currentCell) //derecha o arriba
+    //                {
+    //                    if (newCell - 1 == currentCell) //derecha
+    //                    {
+    //                        board[currentCell].status[3] = true;
+    //                        board[newCell].status[2] = true;
+    //                    }
+    //                    else //arriba
+    //                    {
+    //                        board[currentCell].status[0] = true;
+    //                        board[newCell].status[1] = true;
+    //                    }
+    //                } 
+    //                else
+    //                {
+    //                    if (newCell + 1 == currentCell) //izquierda
+    //                    {
+    //                        board[currentCell].status[2] = true;
+    //                        board[newCell].status[3] = true;
+    //                    }
+    //                    else //abajo
+    //                    {
+    //                        board[currentCell].status[1] = true;
+    //                        board[newCell].status[0] = true;
+    //                    }
+    //                }
+    //                currentCell = newCell;
+    //                num++;
+    //            }
+    //        }
+    //        DungeonGenerator();
+    //    }
+
+    //    private List<int> CheckNeighbors(int cell)
+    //    {
+    //        List<int> neighbors = new List<int>();
+
+    //        if(cell - size.x > 0 && !board[cell - size.x].visited)
+    //        {
+    //            neighbors.Add(cell - size.x); //comprobamos down
+    //        }
+
+    //        if (cell + size.x < board.Count && !board[cell +size.x].visited)// comprobamos top
+    //        {
+    //            neighbors.Add(cell + size.x); //comprobamos up
+    //        }
+
+    //        if (cell % size.x != 0 && !board[cell - 1].visited) // comprobamos left
+    //        {
+    //            neighbors.Add(cell - 1); //comprobamos left
+    //        }
+
+    //        if ((cell + 1) % size.x != 0 && !board[cell + 1].visited) // comprobamos right
+    //        {
+    //            neighbors.Add(cell + 1); //comprobamos right
+    //        }
+
+    //        return neighbors;
+    //    }
+
+    //    private void DungeonGenerator()
+    //    {
+    //        for(int i = 0; i < size.x; i++)
+    //        {
+    //            for(int j = 0; j < size.y; j++)
+    //            {
+    //                var newRoom = Instantiate(room, new Vector3(i * roomSize.x, 0, j * roomSize.y), Quaternion.identity).GetComponent<Room>();
+    //                newRoom.UpdateRoom(board[i + j * size.x].status);
+    //            }
+    //        }
+    //    }
+    // Declaracion de clase interna
     public class Cell
     {
-        public bool visited = false; //Indica si la celda ha sido visitada
-        public bool[] status = new bool[4]; //0 - top; 1- down ; 2 - left; 3 - right
+        public bool visited = false; // Indica si la celda ha sido visitada
+        public bool[] status = new bool[4]; // Estado de las puertas: 0-Top, 1-Down, 2-Left, 3-Right
     }
+    [SerializeField] private Vector2Int size; // Tamaño del mapa (ancho, alto)
+    [SerializeField] public int initPosition = 0; // Posicion inicial en el eje X
+    [SerializeField] private GameObject room; // Prefab de la habitacion
+    [SerializeField] private Vector2 roomSize; // Tamaño de cada habitacion
+    [SerializeField] public int nRooms; // Numero de habitaciones a generar
 
-    [SerializeField] private Vector2Int size;
-    [SerializeField] private GameObject room;
-    [SerializeField] private int initPosition = 0;
-    [SerializeField] private Vector2 roomSize;
-    [SerializeField] private int nRooms;
-
-    List<Cell> board;
+    List<Cell> board; // Lista de celdas del mapa
 
     private void Start()
     {
+        MazeGenerator(); // Generar el mapa al iniciar
+    }
+    public void Generate()
+    {
+        // Elimina las habitaciones generadas anteriormente
+        foreach (Transform child in transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        // Genera el nuevo laberinto
         MazeGenerator();
     }
-
     private void MazeGenerator()
     {
-        board = new List<Cell>();
-
-        for (int i = 0; i < size.x; i++)
+        board = new List<Cell>(); // Inicializar la lista de celdas
+        for (int i = 0; i < size.x; i++) // Crear las celdas del mapa
         {
             for (int j = 0; j < size.y; j++)
             {
-                board.Add(new Cell());
+                board.Add(new Cell()); // Agregar una nueva celda a la lista
             }
         }
 
-        int currentCell = initPosition;
+        int currentCell = initPosition; // Posicion inicial
+        Stack<int> path = new Stack<int>(); // Pila para el camino recorrido
 
-        Stack<int> path = new Stack<int>(); //Pila 
+        int num = 1; // Contador de habitaciones generadas
 
-        int num = 1;
         while (num < nRooms)
         {
-            board[currentCell].visited = true;
+            num++;
+            board[currentCell].visited = true; // Marcar la celda actual como visitada
 
-            //Comprobar vecinos
-            List<int> neighbors = CheckNeighbors(currentCell);
+            List<int> neighbors = CheckNeighbors(currentCell); // Obtener los vecinos no visitados
 
             if (neighbors.Count == 0)
-            { //no hay vecinos
+            {
                 if (path.Count == 0)
                 {
-                    break; //no hay mas celdas en el camino
+                    break; // Si no hay vecinos y el camino esta vacio, terminar
                 }
                 else
                 {
-                    currentCell = path.Pop(); //Pop saca 1 elemento de la fila
+                    currentCell = path.Pop(); // Retroceder al ultimo punto en el camino
                 }
             }
-            else //si hay vecinos
+            else // Hay vecinos no visitados
             {
-                path.Push(currentCell);
+                path.Push(currentCell); // Agregar la celda actual al camino
+                int newCell = neighbors[UnityEngine.Random.Range(0, neighbors.Count)]; // Seleccionar un vecino aleatorio
 
-                int newCell = neighbors[Random.Range(0, neighbors.Count)]; //elegir vecino aleatorio
-
-                if (newCell > currentCell) //derecha o arriba
+                if (newCell > currentCell) // Nueva celda es derecha o arriba
                 {
-                    if (newCell - 1 == currentCell) //derecha
+                    if (newCell - 1 == currentCell) // Derecha
                     {
-                        board[currentCell].status[3] = true;
-                        board[newCell].status[2] = true;
+                        board[currentCell].status[3] = true; // Abrir puerta derecha
+                        board[newCell].status[2] = true; // Abrir puerta izquierda
                     }
-                    else //arriba
+                    else // Arriba
                     {
-                        board[currentCell].status[0] = true;
-                        board[newCell].status[1] = true;
-                    }
-                } 
-                else
-                {
-                    if (newCell + 1 == currentCell) //izquierda
-                    {
-                        board[currentCell].status[2] = true;
-                        board[newCell].status[3] = true;
-                    }
-                    else //abajo
-                    {
-                        board[currentCell].status[1] = true;
-                        board[newCell].status[0] = true;
+                        board[currentCell].status[0] = true; // Abrir puerta arriba
+                        board[newCell].status[1] = true; // Abrir puerta abajo
                     }
                 }
-                currentCell = newCell;
-                num++;
+                else // Izquierda o abajo
+                {
+                    if (newCell + 1 == currentCell) // Izquierda
+                    {
+                        board[currentCell].status[2] = true; // Abrir puerta izquierda
+                        board[newCell].status[3] = true; // Abrir puerta derecha
+                    }
+                    else // Abajo
+                    {
+                        board[currentCell].status[1] = true; // Abrir puerta abajo
+                        board[newCell].status[0] = true; // Abrir puerta arriba
+                    }
+                }
+                currentCell = newCell; // Mover a la nueva celda
             }
         }
-        DungeonGenerator();
-    }
-
-    private List<int> CheckNeighbors(int cell)
-    {
-        List<int> neighbors = new List<int>();
-
-        if(cell - size.x > 0 && !board[cell - size.x].visited)
-        {
-            neighbors.Add(cell - size.x); //comprobamos down
-        }
-
-        if (cell + size.x < board.Count && !board[cell +size.x].visited)// comprobamos top
-        {
-            neighbors.Add(cell + size.x); //comprobamos up
-        }
-
-        if (cell % size.x != 0 && !board[cell - 1].visited) // comprobamos left
-        {
-            neighbors.Add(cell - 1); //comprobamos left
-        }
-
-        if ((cell + 1) % size.x != 0 && !board[cell + 1].visited) // comprobamos right
-        {
-            neighbors.Add(cell + 1); //comprobamos right
-        }
-
-        return neighbors;
+        DungeonGenerator(); // Generar el dungeon basado en el mapa
     }
 
     private void DungeonGenerator()
     {
-        for(int i = 0; i < size.x; i++)
+        for (int i = 0; i < size.x; i++)
         {
-            for(int j = 0; j < size.y; j++)
+            for (int j = 0; j < size.y; j++)
             {
-                var newRoom = Instantiate(room, new Vector3(i * roomSize.x, 0, j * roomSize.y), Quaternion.identity).GetComponent<Room>();
-                newRoom.UpdateRoom(board[i + j * size.x].status);
+                // Instanciar la habitacion en la posicion correspondiente
+                var newRoom = Instantiate(room, new Vector3(i * roomSize.x, 0, j * roomSize.y),
+                    Quaternion.identity, transform).GetComponent<Room>();
+                newRoom.UpdateRoom(board[i + j * size.x].status); // Actualizar las puertas de la habitacion
             }
         }
+    }
+
+    private List<int> CheckNeighbors(int cell)
+    {
+        List<int> neighbors = new List<int>(); // Lista de vecinos no visitados
+
+        if (cell - size.x > 0 && !board[cell - size.x].visited) // Comprobar vecino inferior
+        {
+            neighbors.Add(cell - size.x);
+        }
+        if (cell + size.x < board.Count && !board[cell + size.x].visited) // Comprobar vecino superior
+        {
+            neighbors.Add(cell + size.x);
+        }
+        if (cell % size.x != 0 && !board[cell - 1].visited) // Comprobar vecino izquierdo
+        {
+            neighbors.Add(cell - 1);
+        }
+        if ((cell + 1) % size.x != 0 && !board[cell + 1].visited) // Comprobar vecino derecho
+        {
+            neighbors.Add(cell + 1);
+        }
+        return neighbors; // Devolver la lista de vecinos no visitados
     }
 }
--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -2,29 +2,66 @@ using UnityEngine;
 
 public class Room : MonoBehaviour
 {
-    //Declaracion de variables
-    [SerializeField] private GameObject[] wallDoor; //0 - top; 1- down ; 2 - left; 3 - right
+    ////Declaracion de variables
+    //[SerializeField] private GameObject[] wallDoor; //0 - top; 1- down ; 2 - left; 3 - right
 
 
-    bool connected;
+    //bool connected;
 
+    //public void UpdateRoom(bool[] status)
+    //{
+    //    connected = false;
+
+    //    for(int i = 0; i < status.Length; i++)
+    //    {
+    //        wallDoor[i].SetActive(!status[i]);
+    //        if(status[i])
+    //        {
+    //            connected = true;
+    //        }
+
+    //        if(!connected)
+    //        {
+    //            Destroy(gameObject);
+    //        }
+    //    }
+    //}
+    [Header("Puertas")]
+    [SerializeField] private GameObject[] wallDoor; // 0-Top, 1-Down, 2-Left, 3-Right
+    private bool connected; // Indica si la habitacion esta conectada a otra
+
+    //[Header("Luces para puertas")]
+    //[SerializeField] private GameObject doorLightPrefab; // Prefab de la luz que se instanciara
+    //[SerializeField] private Transform[] doorLightPoints; // Puntos donde se colocaran las luces: 0-Top, 1-Down, 2-Left, 3-Right
+
+    // Metodo para activar las puertas y luces según el estado
     public void UpdateRoom(bool[] status)
     {
-        connected = false;
+        connected = false; // Inicialmente no está conectada
 
-        for(int i = 0; i < status.Length; i++)
+        // Recorrer cada puerta
+        for (int i = 0; i < status.Length; i++)
         {
+            // Activar o desactivar la puerta física
             wallDoor[i].SetActive(!status[i]);
-            if(status[i])
+
+            // Si la puerta está abierta, la habitación está conectada
+            if (status[i])
             {
                 connected = true;
-            }
 
-            if(!connected)
-            {
-                Destroy(gameObject);
+                // Instanciar luz en la puerta si existe prefab y punto asignado
+                //if (doorLightPrefab != null && doorLightPoints != null && doorLightPoints.Length > i && doorLightPoints[i] != null)
+                //{
+                //    Instantiate(doorLightPrefab, doorLightPoints[i].position, Quaternion.identity, transform);
+                //}
             }
         }
-    }
 
+        // Si ninguna puerta está conectada, destruir la habitación
+        if (!connected)
+        {
+            Destroy(gameObject);
+        }
+    }
 }
